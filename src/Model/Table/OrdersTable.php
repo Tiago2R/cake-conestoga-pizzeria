44d3c3de7e9adffb50a94d7eab3c55136eee1e81
Tiago Ribeiro <T2R@Tiagos-MacBook-Air.local>
2015-11-05 21:10:25 -0500

    Login and logout

--- src/Model/Table/OrdersTable.php
+++ src/Model/Table/OrdersTable.php
@@ -2,11 +2,28 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Validation\Validator;
 
 class OrdersTable extends Table
 {
     public function initialize(array $config)
     {
         $this->addBehavior('Timestamp');
+    }
+
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            //->notEmpty('user_id')
+            //->requirePresence('user_id')
+            ->notEmpty('size')
+            ->requirePresence('size');
+
+        return $validator;
+    }
+
+    public function isOwnedBy($orderId, $userId)
+    {
+        return $this->exists(['id' => $orderId, 'user_id' => $userId]);
     }
 }
